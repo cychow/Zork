5d38f6fb6ff8d90da3657bef3580fff933557054
cychow <cchow@purdue.edu>
2015-12-11 01:30:28 -0500

    Done with everyting except attacking, probably should test with other maps

--- src/zork.cpp
+++ src/zork.cpp
@@ -13,6 +13,7 @@
 #ifndef IOSTREAM_H
 #define IOSTREAM_H
 #include <iostream>
+#include <sstream>
 #endif
 
 #include <algorithm>
@@ -24,6 +25,12 @@
 
 void parseCommand(zorkMap * map, gameState * gamestate, std::string lastCommand);
 bool checkTriggers(zorkMap * map, gameState * gamestate, std::string lastCommand, bool interceptCommands);
+
+// list of commands
+void doPut(zorkMap * map, gameState * gamestate, std::vector<std::string> * inputCommandList);
+void doUpdate(zorkMap * map, gameState * state, std::vector<std::string> * inputCommandList);
+void doDelete(zorkMap * map, gameState * state, std::vector<std::string> * inputCommandList);
+void doAdd(zorkMap * map, gameState * state, std::vector<std::string> * inputCommandList);
 
 //using namespace std;
 int main(int argc, char* argv[]) {
@@ -69,6 +76,7 @@ int main(int argc, char* argv[]) {
 	while (!state->won) {
 		std::getline(std::cin, lastCommand);
 		parseCommand(map, state, lastCommand);
+		checkTriggers(map,state,"",true);
 	}
 
 	//delete(map);
@@ -165,26 +173,32 @@ void parseCommand(zorkMap * map, gameState * state, std::string lastCommand) {
 		if (lastCommand.erase(lastCommand.find_last_not_of(" \n\r\t")+1).length() == 4) {
 			std::cout << "Open what?" << std::endl;
 			return;
-
-		} else if (lastCommand.find("open exit")) {
-			if (state->currentRoom->type.compare("exit") == 0) {
-				std::cout << "You have found the exit! You win!" << std::endl;
-			}
-			else {
-				std::cout << "There is no exit in here" << std::endl;
-			}
 		} else {
 			std::string containertoOpen = lastCommand.substr(5);
+			if (!containertoOpen.compare("exit")) {
+				// you want to find the exit
+				if (!state->currentRoom->type.compare("exit")) {
+					std::cout << "Game Over man, Game Over!" << std::endl;
+					state->won = true;
+					return;
+				} else {
+					std::cout << "There's no exit here!" << std::endl;
+					return;
+				}
+
+				return;
+			}
 			if ((map->containerMap.count(containertoOpen)>0)  && (std::find(state->currentRoom->containerList.begin(), state->currentRoom->containerList.end(), containertoOpen) != state->currentRoom->containerList.end())) {
 				auto targetContainer = (map->containerMap)[containertoOpen];
 				std::cout << containertoOpen;
+				targetContainer->isOpen = true;
 				if (targetContainer->itemList.size() != 0) {
 					bool first = true;
 					for (auto iter = targetContainer->itemList.begin(); iter != targetContainer->itemList.end(); ++iter) {
 						if (!first) {
-							first = false;
 							std::cout << ", ";
 						} else {
+							first = false;
 							std::cout << " contains ";
 						}
 						std::cout << (*iter);
@@ -223,13 +237,13 @@ void parseCommand(zorkMap * map, gameState * state, std::string lastCommand) {
 
 		// turn on command
 		if (lastCommand.find("turn on") == 0) {
-			if (lastCommand.erase(lastCommand.find_last_not_fo(" \n\r\t")+1).length() == 7) {
+			if (lastCommand.erase(lastCommand.find_last_not_of(" \n\r\t")+1).length() == 7) {
 				std::cout << "Turn on what?" << std::endl;
 				return;
 			} else {
 				std::string targetItem = lastCommand.substr(8);
 				// read objects in inventory
-				auto iter = std::find(state->inventory->itemList.begin(), state->inventory->itemList.end(), itemToRead);
+				auto iter = std::find(state->inventory->itemList.begin(), state->inventory->itemList.end(), targetItem);
 				if (iter != state->inventory->itemList.end()) {
 					//do whatever the fuck turnon command
 					std::cout << map->itemMap[targetItem]->turnon->print << std::endl;
@@ -242,7 +256,57 @@ void parseCommand(zorkMap * map, gameState * state, std::string lastCommand) {
 
 			}
 		}
-		std::cout << "Error" << std::endl;
+
+		// do a drop
+		if (lastCommand.find("drop") == 0) {
+			if (lastCommand.erase(lastCommand.find_last_not_of(" \n\r\t")+1).length() == 4) {
+				std::cout << "Drop what?" << std::endl;
+				return;
+			} else {
+				std::string targetItem = lastCommand.substr(5);
+				//find objects in inventory
+				auto iter = std::find(state->inventory->itemList.begin(), state->inventory->itemList.end(), targetItem);
+				if (iter != state->inventory->itemList.end()) {
+					//found the item
+					state->inventory->itemList.erase(iter);
+					// put item from room
+					state->currentRoom->itemList.push_back(targetItem);
+					std::cout << targetItem << " dropped." << std::endl;
+					return;
+				} else {
+					std::cout << "You don't have a " << targetItem << " to drop!" << std::endl;
+					return;
+				}
+			}
+		}
+		//TODO make everything do this and parse commands
+		std::vector<std::string> * inputCommandList = new std::vector<std::string>;
+		std::istringstream f(lastCommand);
+		std::string s;
+		while (getline(f, s, ' '))	{
+			inputCommandList->push_back(s);
+		}
+		if (!inputCommandList->at(0).compare("put")) {
+			std::cout << "running put command with '" << lastCommand << "'" << std::endl;
+			doPut(map, state, inputCommandList);
+			return;
+		}
+		if (!inputCommandList->at(0).compare("Update")) {
+			std::cout << "running Update command with '" << lastCommand << "'" << std::endl;
+			doUpdate(map,state,inputCommandList);
+			return;
+		}
+		if (!inputCommandList->at(0).compare("Delete")) {
+			std::cout << "running Delete command with '" << lastCommand << "'" << std::endl;
+			doDelete(map,state,inputCommandList);
+			return;
+		}
+		if (!inputCommandList->at(0).compare("Add")) {
+			std::cout << "running Add command with '" << lastCommand << "'" << std::endl;
+			doAdd(map,state,inputCommandList);
+			return;
+		}
+		std::cout << "Error parsing '" << lastCommand << "'" << std::endl;
 	}
 }
 
@@ -250,26 +314,63 @@ bool checkTriggers(zorkMap * map, gameState * state, std::string lastCommand, bo
 	// Check for triggers
 		// TODO: Check for triggers on items/things in the room
 		std::cout << "Checking for triggers " << std::endl;
+		// create list of every trigger in this room
+		std::vector<zorkTrigger *> * allTriggers = new std::vector<zorkTrigger *>;
 		for (auto trigger = state->currentRoom->triggerList.begin(); trigger != state->currentRoom->triggerList.end(); ++trigger) {
+			allTriggers->push_back(*trigger);
+		}
+		// find triggers in every container in the room
+		for (auto containerIter = state->currentRoom->containerList.begin(); containerIter != state->currentRoom->containerList.end(); ++containerIter) {
+			// find triggers in every item in every container in the room
+			auto currentContainer = map->containerMap[(*containerIter)];
+			for (auto triggerIter = currentContainer->triggerList.begin(); triggerIter != currentContainer->triggerList.end(); ++triggerIter) {
+				allTriggers->push_back(*triggerIter);
+			}
+			for (auto itemIter = currentContainer->itemList.begin(); itemIter != currentContainer->itemList.end(); ++itemIter) {
+				auto currentItem = map->itemMap[(*itemIter)];
+				for (auto triggerIter = currentItem->triggerList.begin(); triggerIter != currentItem->triggerList.end(); ++triggerIter) {
+					allTriggers->push_back(*triggerIter);
+				}
+			}
+		}
+		// find triggers for every item loose in the room
+		for (auto itemIter = state->currentRoom->itemList.begin(); itemIter != state->currentRoom->itemList.end(); ++itemIter) {
+			auto currentItem = map->itemMap[(*itemIter)];
+			for (auto triggerIter = currentItem->triggerList.begin(); triggerIter != currentItem->triggerList.end(); ++triggerIter) {
+				allTriggers->push_back(*triggerIter);
+			}
+		}
+		// find triggers for every creature in the room holy shit yay
+		for (auto creatureIter = state->currentRoom->creatureList.begin(); creatureIter != state->currentRoom->creatureList.end(); ++creatureIter) {
+			auto currentCreature = map->creatureMap[(*creatureIter)];
+			for (auto triggerIter = currentCreature->triggerList.begin(); triggerIter != currentCreature->triggerList.end(); ++triggerIter) {
+				allTriggers->push_back(*triggerIter);
+			}
+		}
+
+
+
+		for (auto trigger = allTriggers->begin(); trigger != allTriggers->end(); ++trigger) {
 			// IF the trigger is not triggered and is of type single, or if the trigger is permanent
-			if ((!((*trigger)->triggered) && !((*trigger)->type).compare("single")) || !((*trigger)->type).compare("permanent")) {
+			if ((!((*trigger)->triggered) && ((*trigger)->type).compare("permanent")) || !((*trigger)->type).compare("permanent")) {
 				bool doTrigger = false;
 				//check if it's an object/status trigger
 				if (!(*trigger)->conditions->object.empty() && !(*trigger)->conditions->status.empty()) {
-					zorkObj * objectOfInterest = NULL;
-					for (auto iter = map->objectList.begin(); iter != map->objectList.end(); ++iter) {
-						if (!(*iter)->name.compare((*trigger)->conditions->object)) {
-							objectOfInterest = *iter;
-							break;
-						}
-					}
-					if (objectOfInterest != NULL) {
-						// if object has the status given by trigger, do something
-						if(!objectOfInterest->status.compare((*trigger)->conditions->status)) {
+					std::string targetObj = (*trigger)->conditions->object;
+					std::string targetStatus = (*trigger)->conditions->status;
+					std::cout << "Checking Obj/Status Trigger: " << targetObj << " | " << targetStatus << std::endl;
+					if (map->objectMap.count(targetObj) > 0) {
+						// found object
+						std::cout << "found " << targetObj << std::endl;
+						auto objIter = map->objectMap[targetObj];
+						if (!(objIter->status.compare(targetStatus))) {
+							std::cout << "^ this trigger is active ^" << std::endl;
+							(*trigger)->triggered = true;
 							doTrigger = true;
+						} else {
+							std::cout << "^ this trigger is inactive ^ status is :" << objIter->status << " needs " << targetStatus << std::endl;
 						}
 					}
-
 				}
 				//or if it's an object/has/owner trigger
 				if (!(*trigger)->conditions->object.empty() && !(*trigger)->conditions->has.empty() && !(*trigger)->conditions->owner.empty()) {
@@ -314,6 +415,7 @@ bool checkTriggers(zorkMap * map, gameState * state, std::string lastCommand, bo
 						}
 						// if you've found the object and the trigger has is true OR if you haven't found the object and the trigger has is 'no'
 						if ((foundObj && !targetHas.compare("yes")) || (!foundObj && !targetHas.compare("no"))) {
+							(*trigger)->triggered = true;
 							doTrigger = true;
 						}
 					} else {
@@ -327,15 +429,19 @@ bool checkTriggers(zorkMap * map, gameState * state, std::string lastCommand, bo
 					}
 					for (auto iter = (*trigger)->actionList.begin(); iter != (*trigger)->actionList.end(); ++iter) {
 						parseCommand(map, state, (*iter));
+						std::cout << "CHECKING TRIGGERS AFTER ACTIVATING THIS COMMAND!" << std::endl;
+						checkTriggers(map, state, "", false);
 					}
 					return true;
-				} else if (doTrigger && (*trigger)->command.empty() && !interceptCommands) {
+				} else if (doTrigger && !interceptCommands) {
 					// do command and do trigger actions by falling through to parsing
 					for (auto iter = (*trigger)->printList.begin(); iter != (*trigger)->printList.end(); ++iter) {
 						std::cout << *iter << std::endl;
 					}
 					for (auto iter = (*trigger)->actionList.begin(); iter != (*trigger)->printList.end(); ++iter) {
 						parseCommand(map, state, (*iter));
+						std::cout << "CHECKING TRIGGERS AFTER ACTIVATING THIS COMMAND!" << std::endl;
+						checkTriggers(map, state, "", false);
 					}
 					return false;
 				}
@@ -343,4 +449,175 @@ bool checkTriggers(zorkMap * map, gameState * state, std::string lastCommand, bo
 			}
 		}
 		return false;
+}
+void doPut(zorkMap * map, gameState * state, std::vector<std::string> * inputCommandList) {
+	if (inputCommandList->size() != 4) {
+		std::cout << "Error" << std::endl;
+		return;
+	} else {
+		// look for item in inventory
+		auto iter = std::find(state->inventory->itemList.begin(), state->inventory->itemList.end(), inputCommandList->at(1));
+		// if has inventory
+		if (!(iter == state->inventory->itemList.end())) {
+			std::cout << "Found " << (*iter) << std::endl;
+			auto iterContainer = std::find(state->currentRoom->containerList.begin(), state->currentRoom->containerList.end(), inputCommandList->at(3));
+			if (!(iterContainer == state->currentRoom->containerList.end())) {
+				std::cout << "Found " << (*iterContainer) << std::endl;
+				auto container = map->containerMap[inputCommandList->at(3)];
+				// make sure you can put it in the container
+				auto iterAcceptedItem = std::find((*container).acceptList.begin(), (*container).acceptList.end(), inputCommandList->at(1));
+				if ((iterAcceptedItem != (*container).acceptList.end()) || ((*container).acceptList.size() == 0)) {
+					state->inventory->itemList.erase(iter);
+					(*container).itemList.push_back(inputCommandList->at(1));
+					std::cout << "Item " << inputCommandList->at(1) << " added to " << inputCommandList->at(3) << "." << std::endl;
+					return;
+				} else {
+					std::cout << "You can't put your " << inputCommandList->at(1) << " in the " << inputCommandList->at(3) << "." << std::endl;
+					return;
+				}
+			} else {
+				std::cout << "There isn't a " << inputCommandList->at(3) << " here." << std::endl;
+			}
+		} else {
+			std::cout << "You don't have a " << inputCommandList->at(1) << "." << std::endl;
+			return;
+		}
+
+	}
+	return;
+}
+
+void doUpdate(zorkMap * map, gameState * state, std::vector<std::string> * inputCommandList) {
+	if (inputCommandList->size() == 4) {
+		if (map->objectMap.count(inputCommandList->at(1)) > 0) {
+			map->objectMap[inputCommandList->at(1)]->status = inputCommandList->at(3);
+			return;
+		} else {
+			std::cerr << "Can't find object " << inputCommandList->at(1) << std::endl;
+		}
+	} else {
+		std::cerr << "Malformed Update command!" << std::endl;
+		return;
+	}
+}
+
+void doAdd(zorkMap * map, gameState * state, std::vector<std::string> * inputCommandList) {
+	if (inputCommandList->size() == 4) {
+		std::string targetObj = inputCommandList->at(1);
+		std::string destObj = inputCommandList ->at(3);
+		std::string destType = "none";
+		if (map->objectMap.count(destObj) > 0) {
+			if (map->containerMap.count(destObj) > 0) {
+				destType = "container";
+			} else if (map->roomMap.count(destObj) > 0) {
+				destType = "room";
+			} else {
+				std::cerr << destObj << " isn't a room or a container!" << std::endl;
+				return;
+			}
+		} else {
+			std::cerr << "Couldn't find destination " << destObj << std::endl;
+			return;
+		}
+		if (map->objectMap.count(targetObj) > 0) {
+			//found the object
+
+			// check if item
+			if (map->itemMap.count(targetObj) > 0) {
+				//is an item
+				if (!destType.compare("container")) {
+					// put it in the container
+					map->containerMap[destObj]->itemList.push_back(targetObj);
+					return;
+				} else {
+					map->roomMap[destObj]->itemList.push_back(targetObj);
+					return;
+				}
+			}
+			// check if container
+			else if (map->containerMap.count(targetObj) > 0) {
+				// is a container
+				if (!destType.compare("room")) {
+					map->roomMap[destObj]->itemList.push_back(targetObj);
+					return;
+				} else {
+					std::cerr << "tried to put container " << targetObj << " in other container " << destObj << std::endl;
+					return;
+				}
+			}
+			// check if creature
+			else if (map->creatureMap.count(targetObj) > 0) {
+				if (!destType.compare("room")) {
+					map->roomMap[destObj]->itemList.push_back(targetObj);
+					return;
+				} else {
+					std::cerr << "tried to put creature " << targetObj << " in container " << destObj << std::endl;
+					return;
+				}
+			} else {
+				std::cerr << targetObj << " isn't an item, container, or creature!" << std::endl;
+				return;
+			}
+			return;
+		} else {
+			std::cerr << "Can't find object " << inputCommandList->at(1) << std::endl;
+			return;
+		}
+	} else {
+		std::cerr << "Malformed Add command!" << std::endl;
+		return;
+	}
+}
+
+void doDelete(zorkMap * map, gameState * state, std::vector<std::string> * inputCommandList) {
+	if (inputCommandList->size() == 2) {
+		std::string targetObj = inputCommandList->at(1);
+		if (map->objectMap.count(targetObj) > 0) {
+			//delete any border references
+			for (auto roomIter = map->roomList.begin(); roomIter != map->roomList.end(); ++roomIter) {
+				// delete any border references
+				for (auto borderIter = (*roomIter)->borderList.begin(); borderIter != (*roomIter)->borderList.end(); ++borderIter) {
+					if (!(*borderIter)->name.compare(targetObj)) {
+						(*roomIter)->borderList.erase(borderIter);
+						break;
+					}
+				}
+				// delete any items in rooms
+				for (auto itemIter = (*roomIter)->itemList.begin(); itemIter != (*roomIter)->itemList.end(); ++itemIter) {
+					if (!(*itemIter).compare(targetObj)) {
+						(*roomIter)->itemList.erase(itemIter);
+						break;
+					}
+				}
+				// delete any containers in rooms
+				for (auto containerIter = (*roomIter)->containerList.begin(); containerIter != (*roomIter)->containerList.end(); ++containerIter) {
+					if (!(*containerIter).compare(targetObj)) {
+						(*roomIter)->containerList.erase(containerIter);
+					}
+				}
+				// delete any items in containers
+				for (auto containerIter = map->containerList.begin(); containerIter != map->containerList.end(); ++containerIter) {
+					for (auto itemIter = (*containerIter)->itemList.begin(); itemIter != (*containerIter)->itemList.end(); ++itemIter) {
+						if (!(*itemIter).compare(targetObj)) {
+							(*containerIter)->itemList.erase(itemIter);
+							break;
+						}
+					}
+				}
+				// delete any creatures in rooms
+				for (auto creatureIter = (*roomIter)->creatureList.begin(); creatureIter != (*roomIter)->creatureList.end(); ++creatureIter) {
+					if (!(*creatureIter).compare(targetObj)) {
+						(*roomIter)->creatureList.erase(creatureIter);
+						break;
+					}
+				}
+			}
+			return;
+		} else {
+			std::cerr << "Can't find object " << inputCommandList->at(1) << std::endl;
+		}
+	} else {
+		std::cerr << "Malformed delete command!" << std::endl;
+		return;
+	}
 }
